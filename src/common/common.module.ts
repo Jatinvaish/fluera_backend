2c0160b99c9043bda82c586e06aee2c54050a054
NiditPatel <criticalgaming2611@gmail.com>
2025-11-13 13:15:30 +0530

    updated the chat service according to user encryption key

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,10 +3,11 @@ import { Module, Global } from '@nestjs/common';
 import { EncryptionService } from './encryption.service';
 import { HashingService } from './hashing.service';
 import { EmailService } from 'src/modules/email-templates/email.service';
+import { ChatEncryptionService } from './chat-encryption.service';
 
 @Global()
 @Module({
-  providers: [EncryptionService, HashingService, EmailService],
-  exports: [EncryptionService, HashingService, EmailService],
+  providers: [EncryptionService, ChatEncryptionService, HashingService, EmailService],
+  exports: [EncryptionService, ChatEncryptionService, HashingService, EmailService],
 })
 export class CommonModule {}
